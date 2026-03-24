8ad73018970ad5ddd32dff461ecd1655c4c8bbe0
Rodrigo Silveira <rggs@cin.ufpe.br>
2026-03-23 21:17:13 -0300

    Update atividade2.asm
    comentando codigo da atividade 2

--- lab2/atividade2.asm
+++ lab2/atividade2.asm
@@ -2,15 +2,19 @@ lw x10,a
 lw x11,b
 lw x12,m
 
-blt x11,x12,soma
-sub x12,x10,x11
+blt x11,x12,soma #if b<m:soma
+sub x12,x10,x11 #else:m=a-b
 bge x0,x0,save
 
-soma:add x12,x10,x11
-save:sw x12,m
+soma:add x12,x10,x11 #soma:m=a+b
+save:sw x12,m #salva o resultado
 
 halt
 
 a: .word 25
 b: .word -24
 m: .word 0
+
+#a=15, b=-6, m=9(x12/a3[00000009])
+#a=14, b=7, m=7 (x12/a3[00000007])
+#a=25, b=-24, m=1(x12/a3[00000001])
