5293feed5a240f561d45d9da0151005932b410de
Rodrigo Silveira <rggs@cin.ufpe.br>
2026-03-23 21:17:48 -0300

    Update atividade3.asm
    comentando codigo da atividade 3

--- lab2/atividade3.asm
+++ lab2/atividade3.asm
@@ -1,20 +1,21 @@
+#carrega os valores genericos declarados
 lw x19, f
 lw x20, g
 lw x21, h
 lw x22, i
 lw x23, j
-beq x22, x23,soma 
-sub x19,x20,x21
-bge x0,x0,end
 
-soma: add x19,x20,x21
-end:sw x19, f
+beq x22, x23,soma #(i==j)?
+sub x19,x20,x21 # else: f=g-h
+bge x0,x0,end #pula para o fim
+
+soma: add x19,x20,x21 #if true: f=g+h
+end:sw x19, f #salva f
 halt
 
+#valores genericos para f,g,h,i,j
 f: .word 0
 g: .word 3
 h: .word 2
 i: .word 5
 j: .word 5
-
-
